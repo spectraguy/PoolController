203bedc8216e88cad10b38b2b413130eebd7d1a1
Benjamin Pelletier <pelletierb@wing.com>
2024-07-06 19:58:23 -0700

    Add basic relay cycling

--- poolcontroller/poolcontroller.ino
+++ poolcontroller/poolcontroller.ino
@@ -1,9 +1,19 @@
 #include "hardware.h"
 
 void setup() {
-  
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println("Initializing...");
+  for (uint8_t p = 0; p < N_RELAY_PINS; p++) {
+    pinMode(relay_pins[p], OUTPUT);
+  }
+  Serial.println("Initialized.");
 }
 
 void loop() {
-  
+  for (uint8_t p = 0; p < N_RELAY_PINS; p++) {
+    digitalWrite(relay_pins[p], HIGH);
+    delay(1000);
+    digitalWrite(relay_pins[p], LOW);
+  }
 }
